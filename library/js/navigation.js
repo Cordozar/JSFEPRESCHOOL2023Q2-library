d413d9cb45896d8c9a73e683f9e9db259632efbf
Yury Samsonenko <cant09y@gmail.com>
2023-08-14 01:34:30 +0300

    feat: add responsive

---
+++ library/js/navigation.js
@@ -0,0 +1,68 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // Navigation
+
+  function navigation() {
+    const burger = document.querySelector('.burger'),
+      navigation = document.querySelector('.nav'),
+      navigationHidden = document.querySelector('.burger_open'),
+      navigationShow = document.querySelector('.burger_close'),
+      navLinks = document.querySelectorAll('.nav__item');
+
+    function burgerClose() {
+      navigation.classList.remove('nav_active');
+      navigationHidden.classList.remove('burger_disablet-img');
+      navigationShow.classList.add('burger_disablet-img');
+    }
+
+    function burgerOpen() {
+      navigation.classList.add('nav_active');
+      navigationHidden.classList.add('burger_disablet-img');
+      navigationShow.classList.remove('burger_disablet-img');
+    }
+
+    function clickerBurger() {
+      navLinks.forEach((el) => {
+        el.addEventListener('click', burgerClose);
+      });
+
+      if (navigation.classList.contains('nav_active')) {
+        burgerClose();
+      } else {
+        burgerOpen();
+      }
+
+      // function close() {
+      //   const timer = setTimeout(() => {
+      //     document.body.addEventListener('click', (e) => {
+      //       if (navigation.classList.contains('nav_active')) {
+      //         console.log('сделано');
+      //         if (!e.target.closest('nav_active')) {
+      //           burgerClose();
+      //         }
+      //       }
+      //     });
+      //   }, 10);
+      //   clearTimeout(timer);
+      // }
+      // close();
+
+      // const timer = setTimeout(() => {
+      //   function close() {
+      //     if (navigation.classList.contains('nav_active')) {
+      //       console.log('сделано');
+      //       if (!e.target.closest('nav_active')) {
+      //         burgerClose();
+      //       }
+      //     }
+      //   }
+
+      //   document.body.addEventListener('click', close);
+      //   document.body.removeEventListener('click', close);
+      // }, 10);
+    }
+
+    burger.addEventListener('click', clickerBurger);
+  }
+
+  navigation();
+});
