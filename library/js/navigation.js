3eafb900ea8ee30debb47a45dcd32eb11bfd1e01
Yury Samsonenko <cant09y@gmail.com>
2023-08-14 19:17:12 +0300

    fix: fix navigation

--- library/js/navigation.js
+++ library/js/navigation.js
@@ -1,6 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-  alert('Проверяющий, задание не доделано до конца, по возможности, прощу сегодня его не проверять, хочу доделать.')
-  
   // Navigation
 
   function navigation() {
@@ -33,34 +31,15 @@ window.addEventListener('DOMContentLoaded', () => {
         burgerOpen();
       }
 
-      // function close() {
-      //   const timer = setTimeout(() => {
-      //     document.body.addEventListener('click', (e) => {
-      //       if (navigation.classList.contains('nav_active')) {
-      //         console.log('сделано');
-      //         if (!e.target.closest('nav_active')) {
-      //           burgerClose();
-      //         }
-      //       }
-      //     });
-      //   }, 10);
-      //   clearTimeout(timer);
-      // }
-      // close();
-
-      // const timer = setTimeout(() => {
-      //   function close() {
-      //     if (navigation.classList.contains('nav_active')) {
-      //       console.log('сделано');
-      //       if (!e.target.closest('nav_active')) {
-      //         burgerClose();
-      //       }
-      //     }
-      //   }
-
-      //   document.body.addEventListener('click', close);
-      //   document.body.removeEventListener('click', close);
-      // }, 10);
+      document.addEventListener('click', (e) => {
+        if (
+          !navigation.contains(e.target) &&
+          !e.target.closest('.burger') &&
+          !e.target.closest('.header__icon-profile')
+        ) {
+          burgerClose();
+        }
+      });
     }
 
     burger.addEventListener('click', clickerBurger);
